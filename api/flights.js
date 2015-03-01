1df1cb203c2f7cf018459ea02c5a86d4e70aaad5
Jonathan Darling <jmdarling@me.com>
2015-03-01 01:47:29 -0500

    Adds filter functions to the flight results.

--- api/flights.js
+++ api/flights.js
@@ -39,6 +39,56 @@ module.exports = function(app) {
     cb(null, data);
   };
 
+  this.filterResults = function(price, distance, results) {
+    // If we have 3 or less results, ignore the filter.
+    if (results.length > 3) {
+      // If the price filter is set, divide the results dataset into 3 groups
+      // and take the group requested.
+      var priceFilteredResults = this.filterResultsByPrice(price, results);
+      var distanceFilteredResults = this.filterResultsByDistance(price, results);
+      var intersectedResults = _.intersection(priceFilteredResults, distanceFilteredResults);
+
+      // Ensure we actually have a result to return.
+      if (!(intersectedResults.length > 0)) {
+        // Disregard the distance filter, it is less important than price.
+        return priceFilteredResults;
+      }
+      return intersectedResults;
+    }
+  };
+
+  this.filterResultsByPrice = function(price, results) {
+    var _ = require('lodash');
+
+    if (price !== 0) {
+      results = _.sortBy(results, 'price');
+      results = _.chunk(results, Math.ceil(results.length / 3))[price - 1];
+    }
+    return results;
+  };
+
+  this.filterResultsByDistance = function(distance, results) {
+    var _ = require('lodash');
+    var airportHelper = require('../helpers/airport');
+
+    if (distance !== 0) {
+      // Calculate a distance for each of the airports.
+      _.forEach(results, function(flight) {
+        flight['distance'] = airportHelper.getDistanceBetweenAirports(flight['departureLocation'], flight['arrivalLocation']);
+      });
+
+      // Sort and chunk the results.
+      results = _.sortBy(results, 'distance');
+      results = _.chunk(results, Math.ceil(results.length / 3))[distance - 1];
+
+      // Remove the distance from the results.
+      _.forEach(results, function(flight) {
+        delete flight['distance'];
+      });
+      return results;
+    }
+  };
+
   app.post('/api/flights', function(req, res) {
     this.verifyRequest(req, function(err, data) {
       if (err) return res.status(400).send(err.msg);
